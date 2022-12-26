9f9b028b10cd3390a47fc1de9c4dac0fab259870
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2022-12-26 14:19:06 +0000

    impl favorite icon

--- app/src/main/java/com/emma_ea/restaurants/RestaurantScreen.kt
+++ app/src/main/java/com/emma_ea/restaurants/RestaurantScreen.kt
@@ -1,6 +1,7 @@
 package com.emma_ea.restaurants
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -9,9 +10,10 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Place
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -55,7 +57,8 @@ fun RestaurantItem(item: Restaurant) {
             modifier = Modifier.padding(8.dp)
         ) {
             RestaurantIcon(Icons.Filled.Place, Modifier.weight(0.15f))
-            RestaurantDetails(item.title, item.description, Modifier.weight(0.85f))
+            RestaurantDetails(item.title, item.description, Modifier.weight(0.7f))
+            FavoriteIcon(Modifier.weight(0.15f))
         }
     }
 }
@@ -80,4 +83,21 @@ fun RestaurantDetails(title: String, description: String, modifier: Modifier) {
             )
         }
     }
+}
+
+@Composable
+fun FavoriteIcon(modifier: Modifier) {
+    var favoriteState by remember { mutableStateOf(false) }
+    val icon = if (favoriteState)
+        Icons.Filled.Favorite
+    else
+        Icons.Filled.FavoriteBorder
+
+    Image(
+        imageVector = icon,
+        contentDescription = "Favorite Restaurant Icon",
+        modifier = modifier
+            .padding(8.dp)
+            .clickable { favoriteState = !favoriteState }
+    )
 }
