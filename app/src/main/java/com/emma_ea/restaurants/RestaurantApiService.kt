95725c6bc00b62c279aa8f8d6ae0907417adbcd0
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2023-01-01 22:48:11 +0000

    impl request to fetch restaurant by id

--- app/src/main/java/com/emma_ea/restaurants/RestaurantApiService.kt
+++ app/src/main/java/com/emma_ea/restaurants/RestaurantApiService.kt
@@ -2,9 +2,13 @@ package com.emma_ea.restaurants
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface RestaurantApiService {
     @GET("restaurants.json")
     suspend fun getRestaurants(): List<Restaurant>
+
+    @GET("restaurants.json/orderBy=\"r_id\"")
+    suspend fun getRestaurant(@Query("equalTo") id: Int): Map<String, Restaurant>
 
 }
