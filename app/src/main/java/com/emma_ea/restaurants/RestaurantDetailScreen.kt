c87d2bdb8ec08679166feac8c41a511fd355f981
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2023-01-01 23:14:20 +0000

    setup detail screen

---
+++ app/src/main/java/com/emma_ea/restaurants/RestaurantDetailScreen.kt
@@ -0,0 +1,40 @@
+package com.emma_ea.restaurants
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+@Composable
+fun RestaurantDetailScreen() {
+    val vm: RestaurantDetailViewModel = viewModel()
+    val item = vm.state.value
+
+    if (item != null) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            RestaurantIcon(
+                icon = Icons.Filled.Place,
+                modifier = Modifier.padding(top = 32.dp, bottom = 32.dp)
+            )
+            RestaurantDetails(
+                title = item.title,
+                description = item.description,
+                modifier = Modifier.padding(bottom = 32.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            )
+            Text(text = "More info coming soon")
+        }
+    }
+}
