55891cc742718efff8024fa72ce8e5ed030de9b4
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2023-01-02 00:06:13 +0000

    fix malformed json exception"

--- app/src/main/java/com/emma_ea/restaurants/RestaurantApiService.kt
+++ app/src/main/java/com/emma_ea/restaurants/RestaurantApiService.kt
@@ -8,7 +8,7 @@ interface RestaurantApiService {
     @GET("restaurants.json")
     suspend fun getRestaurants(): List<Restaurant>
 
-    @GET("restaurants.json/orderBy=\"r_id\"")
+    @GET("restaurants.json?orderBy=\"r_id\"")
     suspend fun getRestaurant(@Query("equalTo") id: Int): Map<String, Restaurant>
 
 }
